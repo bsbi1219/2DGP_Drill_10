f68b4f9ea6d5da5908468dae16a37d769bcf18a0
BAESUBEEN <bsbi1219@naver.com>
2025-11-03 15:35:48 +0900

    bird 날개짓 속도 정하기

--- bird.py
+++ bird.py
@@ -10,6 +10,10 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0) # Meter / Minute
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0) # Meter / Second
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)  # pixel per second
 
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
 class Bird:
     def __init__(self):
         self.image = load_image('bird_animation.png')
